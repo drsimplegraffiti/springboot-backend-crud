e58517970f58448ec4f3d23b80546381bd7436be
drsimplegraffiti <abayomiogunnusi@gmail.com>
2023-01-06 08:56:13 +0100

    Added search query params

--- src/main/java/com/abcode/springbootbackend/repository/EmployeeRepository.java
+++ src/main/java/com/abcode/springbootbackend/repository/EmployeeRepository.java
@@ -2,9 +2,15 @@ package com.abcode.springbootbackend.repository;
 
 import com.abcode.springbootbackend.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // we don't need to use @Repository annotation because we are extending JpaRepository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query("SELECT e FROM Employee e WHERE e.firstName = ?1 OR e.lastName = ?1")
+    List<Employee> findEmployeeByName(String firstName, String lastName);
 
 }
 
--- src/main/java/com/abcode/springbootbackend/service/EmployeeService.java
+++ src/main/java/com/abcode/springbootbackend/service/EmployeeService.java
@@ -3,7 +3,6 @@ package com.abcode.springbootbackend.service;
 import com.abcode.springbootbackend.model.Employee;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface EmployeeService {
     Employee saveEmployee(Employee employee);
@@ -15,4 +14,7 @@ public interface EmployeeService {
     Employee updateEmployeeById(Employee employee, Long id);
 
     void deleteEmployeeById(Long id);
+
+    List<Employee> getByEmployeeName(String firstName, String lastName);
+
 }
